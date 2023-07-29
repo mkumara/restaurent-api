4a3985c98ad1ff4a3dd3e9043cb48847db2cc8bc
mkumara <jayantha@givelify.com>
2023-07-28 21:07:05 -0500

    adding database

--- app/app/tests.py
+++ app/app/tests.py
@@ -14,9 +14,8 @@ class CalcTest(SimpleTestCase):
     def test_add_numbers(self):
         res = calc.add(10, 12)
         self.assertEqual(res, 22)
-    
+        
     def test_subtract_numbers(self):
         """ test calc subtract function"""
         res = calc.subtract(10, 3)
         self.assertEqual(res, 7)
-        
