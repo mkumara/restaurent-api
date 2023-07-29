1bdf0c718cadd66e513306a3338fedd9322bdf2a
mkumara <jayantha@givelify.com>
2023-07-28 21:04:28 -0500

    adding database

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,8 +2,10 @@
 calc program
 """
 
+
 def add(x, y):
     return x + y
+
 
 def subtract(x, y):
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,15 +6,17 @@ Simple Test case
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     """
     Simple calc test
     """
     def test_add_numbers(self):
-        res = calc.add(10,12)
+        res = calc.add(10, 12)
         self.assertEqual(res, 22)
     
     def test_subtract_numbers(self):
         """ test calc subtract function"""
-        res = calc.subtract(10,3)
-        self.assertEqual(res,7)
+        res = calc.subtract(10, 3)
+        self.assertEqual(res, 7)
+        
